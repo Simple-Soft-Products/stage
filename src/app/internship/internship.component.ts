a2ab939d99d1a32827012043c7b20215936e0762
Niels Horeman <niels@sspnet.nl>
2023-12-11 14:22:38 +0100

    +comments voor duidelijkheid

--- src/app/internship/internship.component.ts
+++ src/app/internship/internship.component.ts
@@ -1,33 +1,39 @@
-import { Component } from '@angular/core';
-import { interval } from 'rxjs';
+import { Component } from "@angular/core"
+import { interval } from "rxjs"
 
 @Component({
-  selector: 'app-internship',
-  templateUrl: './internship.component.html',
-  styleUrls: ['./internship.component.scss']
+  selector: "app-internship",
+  templateUrl: "./internship.component.html",
+  styleUrls: ["./internship.component.scss"],
 })
 export class InternshipComponent {
-	remainingTime = {
-		days: 0,
-		hours: 0,
-		minutes: 0,
-		seconds: 0
-	}
+  // Default object. Deze wordt gebruikt om de countdown te laten zien.
+  remainingTime = {
+    days: 0,
+    hours: 0,
+    minutes: 0,
+    seconds: 0,
+  }
 
-	private endTime = new Date('02-06-2023 09:00')
+  // De einddatum van de stage. Deze wordt gebruikt om het verschil te gaan berekenen.
+  private endTime = new Date("12-31-2023 09:00")
 
-	private $interval = interval(1000).subscribe(() => this.getRemainingTime())
+  // De interval die elke seconde de countdown functie gaat aanroepen.
+  private $interval = interval(1000).subscribe(() => this.getRemainingTime())
 
-	ngOnInit() {
-		this.getRemainingTime()
-	}
+  // De constructor wordt gebruikt om de countdown te starten.
+  ngOnInit() {
+    this.getRemainingTime()
+  }
 
-	ngOnDestroy() {
-		this.$interval.unsubscribe()
-	}
+  // De ngOnDestroy wordt gebruikt om de interval te stoppen.
+  ngOnDestroy() {
+    this.$interval.unsubscribe()
+  }
 
-	getRemainingTime = () => {
-		// Maak hier de countdown functie
-	}
-
+  // Deze functie wordt elke seconde aangeroepen en berekent het verschil tussen de einddatum en de huidige datum.
+  getRemainingTime = () => {
+    // Maak hier de countdown functie
+    // Tip: gebruik de endTime variabele
+  }
 }
